adca53bd823d7cf73de3db49701e59043d43134c
Ishan1998 <rathore.ishan@gmail.com>
2023-04-25 11:16:09 -0400

    Updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "";
+  "https://github.com/Ishan1998/MyPortfolio/tree/main/src/Assets/Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
